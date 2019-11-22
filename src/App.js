915b7579abba3fa2215e58b664dfdfccba4e5f61
renburnett <renardburnett@gmail.com>
2019-11-22 12:24:45 -0800

    routes working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Fridge from './pages/Fridge';
+import Account from './pages/Account';
 import FridgesContainer from './pages/FridgesContainer';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
@@ -9,11 +10,13 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
       <Router>
-        <Login />
-        <FridgesContainer />
-        <Fridge />  
+        <Navbar />
+        <Route path='/login' render={() => <Login /> }/>
+        <Route path='/account' component={() => <Account />}/>
+        <Route exact path='/' render={ () => <FridgesContainer /> }/>
+        <Route path='/fridges' render={ () => <FridgesContainer /> }/>
+        <Route path='/fridges/:id' render={ () => <Fridge /> }/>
       </Router>
     </div>
   );
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
 
-class Login extends Component {
-  
-  state = {
-    loggedIn: false,
-    currentUser: null
-  }
+class Account extends Component {
 
   componentDidMount() {
     //TODO: refactor to pull GRID out of login AND FridgesContainer?
@@ -18,12 +13,9 @@ class Login extends Component {
         <Grid.Column style={{ maxWidth: '80vh'}}>
 
           <Form size='large'>
-            <Segment>
-              <Header as='h1' color='blue' textAlign='left'>
-                Grasher
-                <Header.Subheader>
-                  we don't let your groceries expire on you!
-                </Header.Subheader>
+            <Segment stackable>
+              <Header as='h2' color='blue' textAlign='left'>
+                Change Account Info
               </Header>
 
               <Form.Input fluid icon='user' iconPosition='left' placeholder='maria@nw_harvest.org' />
@@ -35,11 +27,8 @@ class Login extends Component {
                 type='password'
               />
               <Button color='teal' fluid size='large'>
-                Login
+                Submit
               </Button>
-              <Message>
-                Never been here before? <a href='#'>Sign Up!</a>
-              </Message>
             </Segment>
           </Form>
         </Grid.Column>
@@ -48,4 +37,4 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default Account;
